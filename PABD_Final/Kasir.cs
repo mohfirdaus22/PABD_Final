c3545d60afa0b9930bd7d9b582a19645d0cb484a
mohfirdaus22 <thepashter6@gmail.com>
2023-07-11 15:20:01 +0700

    Add fungtion datagrid form kasir

--- PABD_Final/Kasir.cs
+++ PABD_Final/Kasir.cs
@@ -31,6 +31,18 @@ namespace PABD_Final
 
         }
 
+        //buat datagrid
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string query = "SELECT Id_kasir, Nama, Email, No_telp FROM dbo.Kasir";
+            SqlDataAdapter da = new SqlDataAdapter(query, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
+
         public Kasir()
         {
             InitializeComponent();
