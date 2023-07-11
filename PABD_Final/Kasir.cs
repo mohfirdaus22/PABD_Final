57b4396afc3a0cc9338a2e7490769f27fbc77d24
mohfirdaus22 <thepashter6@gmail.com>
2023-07-11 15:24:18 +0700

    Add fungtion button save di Kasir

--- PABD_Final/Kasir.cs
+++ PABD_Final/Kasir.cs
@@ -74,5 +74,48 @@ namespace PABD_Final
             btnClear.Enabled = true;
 
         }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string idkasir = txtidkasir.Text;
+            string nama = txtnama.Text;
+            string email = txtemail.Text;
+            string notelp = txtnotelp.Text;
+
+            if (idkasir == "")
+            {
+                MessageBox.Show("Masukkan Id Kasir", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (nama == "")
+            {
+                MessageBox.Show("Masukkan Nama", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (notelp == "")
+            {
+                MessageBox.Show("Masukkan No Telp", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (email == "")
+            {
+                MessageBox.Show("Masukkan Email", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "INSERT INTO Kasir (Id_kasir, Nama, Email,No_telp) VALUES (@id_kasir, @nama, @email, @No_telp)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("@Id_koki", idkasir));
+                cmd.Parameters.Add(new SqlParameter("@Alamat", email));
+                cmd.Parameters.Add(new SqlParameter("@No_telp", notelp));
+                cmd.Parameters.Add(new SqlParameter("@Nama", nama));
+
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
+            }
+        }
     }
 }
