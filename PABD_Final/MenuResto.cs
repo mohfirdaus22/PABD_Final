c632bc503ca987ac71b1b2b5a7285189fa2f81e2
mohfirdaus22 <thepashter6@gmail.com>
2023-07-11 19:45:02 +0700

    Add fungtion button Save Menuresto

--- PABD_Final/MenuResto.cs
+++ PABD_Final/MenuResto.cs
@@ -61,6 +61,7 @@ namespace PABD_Final
             btnOpen.Enabled = false;
         }
 
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
             txtkodemenu.Enabled = true;
@@ -71,6 +72,49 @@ namespace PABD_Final
             btnOpen.Enabled = true;
             btnClear.Enabled = true;
 
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string kodemenu = txtkodemenu.Text;
+            string nama = txtnama.Text;
+            string stok = txtstok.Text;
+            string harga = txtharga.Text;
+
+            if (kodemenu == "")
+            {
+                MessageBox.Show("Masukkan Kode Menu", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (nama == "")
+            {
+                MessageBox.Show("Masukkan Nama", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (stok == "")
+            {
+                MessageBox.Show("Masukkan stok", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (harga == "")
+            {
+                MessageBox.Show("Masukkan Harga", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "INSERT INTO Menu (Kode_menu, Nama, Stok,Harga) VALUES (@kode_menu, @nama, @stok, @harga)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("@Kode_menu", kodemenu));
+                cmd.Parameters.Add(new SqlParameter("@Stok", stok));
+                cmd.Parameters.Add(new SqlParameter("@Harga", harga));
+                cmd.Parameters.Add(new SqlParameter("@Nama", nama));
+
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
+            }
         }
     }
 }
